1710c2911c15543e0dbed477652ece20f5013057
taoka <mpr1q7z@gmail.com>
2014-11-10 20:57:24 +0900

    add controller spec

---
+++ spec/controllers/blogs_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe BlogsController, :type => :controller do
+  describe "GET #index" do
+    it "@blogsに全てのBlogが入っていること" do
+      get :index
+      @blogs = Blog.all
+      expect(assigns(:blogs)).to eq @blogs
+    end
+  end
+
+  describe "POST #create" do
+    before do
+      post :create, { blog: {title: 'test'} }
+    end
+    it "新規作成後に@blogのshowに遷移すること" do
+      @blog = Blog.last
+      response.should redirect_to blog_path(@blog)
+    end
+  end
+end
